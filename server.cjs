3767ca1bd61a45fe69aac086eec945a161463e1f
bexruzilxomuv805-ship-it <bexruzilxomuv805@gmail.com>
2026-08-16 14:32:55 +0500

    fix render server port

---
+++ server.cjs
@@ -0,0 +1,14 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("src/data/db.json");
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, "0.0.0.0", () => {
+  console.log(`JSON Server running on port ${port}`);
+});
